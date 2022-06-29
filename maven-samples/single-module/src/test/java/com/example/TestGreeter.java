94be51384bd76bef9403913479d71aa57b7fa12e
Abitha-20 <abimanoharanhere@gmail.com>
2022-06-29 07:30:19 +0530

    changes are made

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -36,8 +36,9 @@ public class TestGreeter {
 
     @Test
   public void greetShouldIncludeGreetingMessage() {
-    String someone = "World, Thanks you making me Happy";
+    String someone = "This is ABI, Thankyou for making me Happy";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
+
 }
